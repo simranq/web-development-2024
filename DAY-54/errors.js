880c49b471a02d8875fde6c9b7380665689fe870
simranq <qsimran08@gmail.com>
2024-11-03 11:32:28 +0530

    DAY 54

--- DAY-54/errors.js
+++ DAY-54/errors.js
@@ -1,6 +1,45 @@
 const fs = require('fs');
 function readFile() {
+    try {
     const fileData = fs.readFileSync('data.json');
+    }catch{
+        console.log('An error has occurred.');
+    }
+    console.log(fileData);
     console.log('Hi there !');
 };
+
 readFile();
+
+// Why not to use try catch for the whole program?
+// Using try-catch blocks for the entire program can hide specific errors, making debugging difficult. 
+// It also adds unnecessary performance overhead and makes your code harder to maintain.
+//  It's better to handle errors locally where they occur.
+// Also each nd every error has to be handled differently than the other.
+// syntax -
+// try {
+//     somethingThatMightFail();
+// } catch (error) {   // accept an "error parameter after catch
+//     console.log(error.message);
+// }
+
+// function doSomething() {
+//     // do smthng
+//     throw { message: 'Something went wrong !'};
+// }
+
+
+//VARIABLE SCOPING
+const fs = require('fs');
+function readFile() {
+    let fileData;
+    try {
+    fileData = fs.readFileSync('data.json');
+    }catch{
+        console.log('An error has occurred.');
+    }
+    console.log(fileData);
+    console.log('Hi there !');
+};
+
+
