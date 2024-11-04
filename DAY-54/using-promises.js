1b688bc47aa1bc01175331710525383395e5b72a
simranq <qsimran08@gmail.com>
2024-11-04 10:58:44 +0530

    added async

--- DAY-54/using-promises.js
+++ DAY-54/using-promises.js
@@ -1,13 +1,13 @@
 const { error } = require('console');
 const fs = require('fs/promises');
-function readFile() {
+async function readFile() {
     let fileData;
-
-    fs.readFile('NOTES.txt').then (function (fileData) {
-    console.log('File parsing done ! ');
-    console.log(fileData.toString());
-    //return anotherAsyncOperation;
-});
+    // fs.readFile('NOTES.txt').then (function (fileData) {
+    fileData = await fs.readFile('NOTES.txt'){
+        console.log('File parsing done ! ');
+        console.log (fileData.toString());
+        //return anotherAsyncOperation; 
+};
     //.then(function (){});
     //.catch( function (error) {
         // console.log(error);
@@ -20,4 +20,4 @@ function readFile() {
 readFile();
 //same o/p but advantage is more structured code, you can add more async opns like line 8-9 above.
 //errors could occur, we can't use try, catch, callback function here. Instead use error parameter in async
-//for errors in promises catch
+//for errors in promises try, catch
